fd83fdc5704c9eb9152d38507a114b4228d16809
= <rondinelibrt18@gmail.com>
2023-02-05 13:43:04 -0300

    first commit
    Co-authored-by: 4-Leafs-Code <4-Leafs-Code@users.noreply.github.com>

---
+++ src/styles/colors.js
@@ -0,0 +1,20 @@
+export default {
+  TEXT_CYAN: '#00A5B5',
+  TEXT_LT_CYAN: '#0AA4B2',
+  TEXT_PINK: '#F80FC6',
+  TEXT_LT_PINK: '#FA31CF',
+  TEXT_GREEN: '#159B22',
+  TEXT_LT_GREEN: '#159B22',
+  TEXT_PURPLE: '#9F54E6',
+  TEXT_LT_PURPLE: '#9F54E6',
+  SKILLS_PURPLE: '#DA00EE',
+}; // CAN YOU HEAR ME? 
+// 
+
+export const hero_styles = {
+  GREEN_HERO_TEXT: '#35FF49',
+  PURPLE_HERO_TEXT: '#9F54E6',
+  FEARLESSTECH_PURPLE: '#B400C5', // background: linear-gradient(180deg, #B400C5 0%, #0BAFBE 100%);
+  FEARLESSTECH_CYAN: '#0BAFBE',
+}
+// I GUESS ITS ENOUGH, if we need something else we can place
