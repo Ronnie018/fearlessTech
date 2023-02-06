e92fb34c97ccebce8c1b9ed1b907e7a73db97139
= <rondinelibrt18@gmail.com>
2023-02-05 23:34:26 -0300

    style: styled hero-section and landing page text
    Co-authored-by: 4-Leafs-Code <4-Leafs-Code@users.noreply.github.com>

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -7,7 +7,7 @@ export default {
   TEXT_LT_GREEN: '#08C91C',
   TEXT_PURPLE: '#9F54E6',
   TEXT_LT_PURPLE: '#8617EF',
-  SKILLS_PURPLE: '#DA00EE', 
+  SKILLS_PURPLE: '#DA00EE',
 };
 
 export const hero_styles = {
@@ -15,4 +15,25 @@ export const hero_styles = {
   PURPLE_HERO_TEXT: '#9F54E6',
   FEARLESSTECH_PURPLE: '#B400C5', // background: linear-gradient(180deg, #B400C5 0%, #0BAFBE 100%);
   FEARLESSTECH_CYAN: '#0BAFBE',
+};
+
+export const intro_styles = {
+  TEXT_CYAN: '#00A5B5',
+  TEXT_LT_CYAN: '#0AA4B2', // skills hover
+  SKILLS_PURPLE: '#DA00EE',
+};
+
+export const mission_styles = {
+  TEXT_PINK: '#F80FC6',
+  TEXT_LT_PINK: '#FA31CF',
 }
+
+export const join_styles = {
+  TEXT_GREEN: '#159B22',
+  TEXT_LT_GREEN: '#08C91C',
+};
+
+export const pitch_styles = {
+  TEXT_PURPLE: '#9F54E6',
+  TEXT_LT_PURPLE: '#8617EF',
+};
